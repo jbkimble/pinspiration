e6896df30ded6a87a888314fd5d265662d337087
Anna Dolan <anna.dolan@gmail.com>
2016-12-13 18:09:07 -0700

    Tests and some functionality for sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      flash[:success] = "Successfully logged in as #{@user.name}!"
+      session[:user_id] = @user.id
+      redirect_to dashboard_path
+    else
+      flash.now[:error] = "Login failed, please try again."
+      render :new
+    end
+  end
+end
