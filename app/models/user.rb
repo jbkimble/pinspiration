fed1f3cdc6b1875c9f54e316a1ecd5e3a473a139
Anna Dolan <anna.dolan@gmail.com>
2016-12-14 13:58:30 -0700

    Add initial work on authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+
   validates :name, presence: true
   validates :phone, presence: true
   validates :username, presence: true, uniqueness: true
@@ -7,4 +8,12 @@ class User < ApplicationRecord
 
   has_many :roles, through: :user_roles
   has_many :user_roles
+
+  def registered?
+    roles.exists?(name: "user")
+  end
+
+  def admin?
+    roles.exists?(name: "admin")
+  end
 end
---
+++ app/services/permissions_service.rb
@@ -0,0 +1,34 @@
+class PermissionsService
+  def initialize(user, controller, action)
+    @action = action
+    @controller = controller
+    @user = user || User.new
+  end
+
+  def allow?
+    if user.admin?
+      admin_permissions
+    elsif user.registered?
+      user_permissions
+    else
+      guest_permissions
+    end
+  end
+
+  private
+    attr_reader :action, :controller, :user
+
+    def admin_permissions
+      return true if controller == "sessions" && action.in?(%w(index new create destroy))
+      return true if controller == "users" && action.in?(%w(show))
+    end
+
+    def user_permissions
+      return true if controller == "sessions" && action.in?(%w(index new create destroy))
+      return true if controller == "users" && action.in?(%w(show))
+    end
+
+    def guest_permissions
+      return true if controller == "sessions" && action.in?(%w(index new))
+    end
+end
