370cbf24373527c9d7a21aadf87eef9189be36e6
Jon Kimble <jonathanbkimble@gmail.com>
2016-12-14 05:15:03 -0700

    Add create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,22 @@ class UsersController < ApplicationController
 
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome #{@user.name}"
+      session[:user_id] = @user.id
+      redirect_to dashboard_path
+    else
+      flash[:failure] = "Account creation failed, please try again"
+      redirect_to root_path
+    end
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :username, :password, :phone)
