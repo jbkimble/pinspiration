76060840fcb3882309bdadc4fcab8f5cf15ee91f
Anna Dolan <anna.dolan@gmail.com>
2016-12-15 16:42:22 -0700

    No failing tests

--- spec/features/sessions/user_can_log_out_spec.rb
+++ spec/features/sessions/user_can_log_out_spec.rb
@@ -4,7 +4,11 @@ describe "Logged in user can log out" do
 
   scenario "A logged in user clicks the logout button" do
     user = create(:user)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    visit '/'
+    click_on "Login"
+    fill_in "email", with: "#{user.email}"
+    fill_in "password", with: "#{user.password}"
+    click_button "Login"
 
     visit root_path
 
