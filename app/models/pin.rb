4b383c2849333b5f5a7108d7e36909ff7c8b7575
Erin Pintozzi <erin.pintozzi@gmail.com>
2016-12-15 14:19:02 -0700

    Create pin validations Add basic divs to pin views

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,2 +1,8 @@
 class Pin < ApplicationRecord
+  validates :name, presence: true
+  validates :source, presence: true
+  validates :image, presence: true
+
+  has_many :boards
+  has_many :comments
 end
