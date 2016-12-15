3728a4014dcd1eaa0bf00a24ac60b859d5ad77c5
Anna Dolan <anna.dolan@gmail.com>
2016-12-15 14:18:53 -0700

    Fix failing tests

--- spec/features/user/user_can_create_account_spec.rb
+++ spec/features/user/user_can_create_account_spec.rb
@@ -7,7 +7,7 @@ describe 'As a User' do
       visit root_path
       click_on 'Create Account'
 
-      fill_in 'Avatar', with: 'https://images-na.ssl-images-amazon.com/images/G/01/img15/pet-products/small-tiles/23695_pets_vertical_store_dogs_small_tile_8._CB312176604_.jpg'
+      #fill_in 'Avatar', with: 'https://images-na.ssl-images-amazon.com/images/G/01/img15/pet-products/small-tiles/23695_pets_vertical_store_dogs_small_tile_8._CB312176604_.jpg'
       fill_in 'Username', with: 'Bilbo'
       fill_in 'Email', with: 'b@shire.com'
       fill_in 'Name', with: 'Bilbo Baggins'
