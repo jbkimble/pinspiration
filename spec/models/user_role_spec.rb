dc575a993594ee414bcd665f3c5bf50296b97e25
Anna Dolan <anna.dolan@gmail.com>
2016-12-15 17:09:04 -0700

    All model tests passing

--- spec/models/user_role_spec.rb
+++ spec/models/user_role_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe UserRole, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "relationships" do
+    it "belongs to a user" do
+      user_role = create(:user_role)
+      expect(user_role).to respond_to(:user)
+    end
+
+    it "belongs to a role" do
+      user_role = create(:user_role)
+      expect(user_role).to respond_to(:role)
+    end
+  end
 end
