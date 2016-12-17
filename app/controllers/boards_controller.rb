e04a9e42fc5e517aada2f7ea980424faafd9915b
Erin Pintozzi <erin.pintozzi@gmail.com>
2016-12-17 15:26:14 -0700

    Create update functionality for boards Write feature test for updating a board Add view for editing a board

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -7,13 +7,28 @@ class BoardsController < ApplicationController
   def create
     @board = Board.create(board_params)
     @board.user = current_user
-    # binding.pry
     if @board.save
       flash[:success] = "You have successfully create the board #{@board.name}."
       redirect_to user_board_path(@board.user.slug, @board.slug)
     else
       flash[:failure] = "Board creation failed, please try again"
       redirect_to new_board_path
+    end
+  end
+
+  def edit
+    @board = Board.find(params[:id])
+  end
+
+  def update
+    @board = Board.find(params[:id])
+    @board.update(board_params)
+    if @board.save
+      flash[:success] = "You have successfully updated this board."
+      redirect_to user_board_path(@board.user.slug, @board.slug)
+    else
+      flash[:failure] = "Board update failed, please try again"
+      redirect_to edit_board_path(@board.user.slug, @board.slug)
     end
   end
 
