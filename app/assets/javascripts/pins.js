ad2305f80b20728d3640561bf30bfa6f591d4f67
Anna Dolan <anna.dolan@gmail.com>
2016-12-18 12:49:43 -0700

    Masonry finally working properly

--- app/assets/javascripts/pins.js
+++ app/assets/javascripts/pins.js
@@ -1,10 +1,11 @@
-
+$(document).ready(function (){
   var $grid = $('.grid').masonry({
     itemSelector: '.grid-item',
-
     percentPosition: true,
-    columnWidth: '.grid-sizer'
+    columnWidth: 260,
+    gutter: 30,
     });
-    $('grid').imagesLoaded().progress( function() {
-      $grid.masonry();
+  $('grid').imagesLoaded().progress( function() {
+      $grid.masonry('layout');
       });
+});
