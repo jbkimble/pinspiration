ccdd466dc89dc16df987ed89159fd47e345624b5
Vido <vidoseaver@gmail.com>
2016-12-18 11:00:42 -0700

    Add visitor views all pins feature test

--- spec/features/user/visitor_views_all_pins_in_chronological_order_spec.rb
+++ spec/features/user/visitor_views_all_pins_in_chronological_order_spec.rb
@@ -4,13 +4,19 @@ describe "As a visitor" do
   context "I visit the root path" do
     scenario "and I see all the pins" do
       pins = create_list(:pin, 10)
-      second_newest_pin = create(:pin name:"second newest pin")
+      second_newest_pin = create(:pin, name:"second newest pin")
       newest_pin = create(:pin, name:"newest pin")
-
+        binding.pry    
       visit root_path
 
-      expect
+      expect(page).to have_selector(".pin", count:12)
 
+      within(".pin:first") do
+        expect(page).to have_content("newest pin")
+      end
+      within("pin:second") do
+        expect(page).to have_content("second newest pin")
+      end
     end
   end
 end
