cdcf9252bee3d7ff616b853c770f797d53a2acd4
Erin Pintozzi <erin.pintozzi@gmail.com>
2016-12-17 21:20:12 -0700

    Begin work on saving pin to a board Add logic to pin controller/ new to direct user Add "save pin" button to pin show view

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -8,4 +8,12 @@ class PinsController < ApplicationController
     @pin = Pin.find(params[:id])
   end
 
+  def new
+    if !current_user.nil? && (current_user.registered? || current_user.admin?)
+      redirect_to root_path
+    else
+      redirect_to login_path
+    end
+  end
+
 end
