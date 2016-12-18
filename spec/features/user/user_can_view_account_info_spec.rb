f6282813a927ae349383bac703ce316f20940439
Anna Dolan <anna.dolan@gmail.com>
2016-12-17 18:36:03 -0700

    Test for clicking cancel on profile edit

--- spec/features/user/user_can_view_account_info_spec.rb
+++ spec/features/user/user_can_view_account_info_spec.rb
@@ -5,7 +5,6 @@ describe 'As a logged in user' do
     role = create(:role, id: 2, name: "user")
     visit new_user_path
 
-    #fill_in 'Avatar', with: 'https://images-na.ssl-images-amazon.com/images/G/01/img15/pet-products/small-tiles/23695_pets_vertical_store_dogs_small_tile_8._CB312176604_.jpg'
     fill_in 'Username', with: 'Bilbo'
     fill_in 'Email', with: 'b@shire.com'
     fill_in 'Name', with: 'Bilbo Baggins'
@@ -28,6 +27,19 @@ describe 'As a logged in user' do
 
       expect(current_path).to eq(profile_path)
       expect(page).to have_xpath("//img[contains(@src,'bilbo.jpg')]")
+    end
+
+    scenario 'But I cancel my changes' do
+      user = User.first
+
+      visit edit_user_path(user)
+
+      fill_in "Avatar", with: "bilbo.jpg"
+
+      click_on "Cancel"
+
+      expect(current_path).to eq(profile_path)
+      expect(page).to_not have_xpath("//img[contains(@src,'bilbo.jpg')]")
     end
   end
 end
