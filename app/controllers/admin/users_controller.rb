79d4f543d242d6ec0a54f8c594d2310a438bb9b4
Erin Pintozzi <erin.pintozzi@gmail.com>
2016-12-20 15:52:49 -0700

    Admin can see list of users Admin can click on username to go to user's profile Create admin/user show view Write feature test for the above

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,6 +5,7 @@ class Admin::UsersController < Admin::BaseController
   end
 
   def show
+    @user = User.find(params[:id])  
   end
 
 end
--- spec/features/admins/users/admin_can_see_list_of_users_spec.rb
+++ spec/features/admins/users/admin_can_see_list_of_users_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
-describe "admin sees list of users" do
-  scenario "an admin can see a list of users and view their profiles" do
+describe "admin sees users" do
+  scenario "an admin can see a list of users" do
     role = Role.create(name: "admin")
     admin = create(:user, roles:[role])
     user1, user2, user3 = create_list(:user, 3)
@@ -10,8 +10,23 @@ describe "admin sees list of users" do
 
     visit admin_users_path
 
-    expect(page).to have_content(user1.name)
-    expect(page).to have_content(user2.name)
-    expect(page).to have_content(user3.name)
+    expect(page).to have_content(user1.username)
+    expect(page).to have_content(user2.username)
+    expect(page).to have_content(user3.username)
   end
+  scenario "an admin can click on a user and view their content" do
+    role = Role.create(name: "admin")
+    admin = create(:user, roles:[role])
+    user1, user2, user3 = create_list(:user, 3)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit admin_users_path
+
+    click_on user1.username
+
+    expect(current_path).to eq(admin_user_path(user1.id))
+
+  end
+
 end
